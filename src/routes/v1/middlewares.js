bdc3eb116519752457f9ddf80f39b79c1986ea49
Albert Chan <atfchan@gmail.com>
2016-10-16 01:37:20 +0800

    Fix middleware for CORS

--- src/routes/v1/middlewares.js
+++ src/routes/v1/middlewares.js
@@ -6,8 +6,10 @@ const router = express.Router();
 router.use((req, res, next) => {
   console.log('[API]:', req.url);
 
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
